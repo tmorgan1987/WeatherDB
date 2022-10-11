dac254ab38b6bfb26160f6cc4c4c71df0b357758
tmorgan1987 <tmorgan1987@outlook.com>
2022-10-10 22:25:17 -0400

    shit works

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const apiKey = "8d263a2aa699cd8bdfaba1edd5f94798";
 const today = moment().format("M, D, YYYY");
 const date = document.getElementById("date");
 const town = document.getElementById("town");
+const currentWeatherIcon = document.getElementById("current-weather-icon");
+const todayTemperature = document.getElementById("today-temperature");
+const todayWind = document.getElementById("today-wind");
+const todayHumidity = document.getElementById("today-humidity");
 
 
 const citySearch = () => {
@@ -17,7 +21,7 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&appid=${ap
 		let lat = res[0].lat;
 		let lon = res[0].lon;
 		console.log(lat, lon);
-		fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+		fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
 			.then((res)=>{
 				return res.json();
 			})
@@ -34,11 +38,15 @@ const renderWeatherData = (city) => {
 	console.log(city.city.name);
 	town.textContent = city.city.name;
 	date.textContent = today;
+	const iconUrl = `https://openweathermap.org/img/w/${city.list[0].weather[0].icon}.png`
+	currentWeatherIcon.setAttribute("src", iconUrl);
+	todayTemperature.textContent = "Temperature: " + city.list[0].main.temp;
+	todayWind.textContent = "Wind: " + city.list[0].wind.speed;
+	todayHumidity.textContent = "Humidity: " + city.list[0].main.humidity;
 }
 
 
-
-
+// (city.list[0].weather[0].icon);
 
 
 
